05917777dfefd114943ead4644d7380d46b809c3
Luka Janketic <luka.janketic@gmail.com>
2022-11-10 15:27:54 +0100

    Add name to array, name to uppercase

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const app = Vue.createApp({
-
+    data() {
+        return {
+            newPlayerInput: "",
+            playerList: []
+        }
+    },
+    methods: {
+        addPlayer() {
+            const nameToUpper = this.newPlayerInput[0].toUpperCase() + this.newPlayerInput.slice(1)
+            this.playerList.push(nameToUpper)
+            this.newPlayerInput = ""
+            console.log('Current player list: ', this.playerList)
+        }
+    }
 })
 
+app.mount('#scoreboard-section')
