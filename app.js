0cf5be3c966bb8f69dd72196f97010d55251b981
Luka Janketic <luka.janketic@gmail.com>
2022-11-11 10:41:57 +0100

    added a win condition

--- app.js
+++ app.js
@@ -2,19 +2,31 @@ const app = Vue.createApp({
     data() {
         return {
             newPlayerInput: "",
-            playerList: []
+            playerList: [],
+            maxScore: 5
         }
     },
     methods: {
         addPlayer() {
-            const nameToUpper = this.newPlayerInput[0].toUpperCase() + this.newPlayerInput.slice(1)
-            this.playerList.push({Name: nameToUpper, Score: 0})
-            this.newPlayerInput = ""
+            if (this.newPlayerInput !== "") {
+                const nameToUpper = this.newPlayerInput[0].toUpperCase() + this.newPlayerInput.slice(1)
+                this.playerList.push({ Name: nameToUpper, Score: 0 })
+                this.newPlayerInput = ""
+            } else {
+                alert('Please enter a valid player name!')
+            }
         },
         resetLobby() {
             alert("Lobby reset!")
             this.playerList = []
             this.newPlayerInput = ""
+        },
+        endGame(player) {
+            if (player.Score === 5) {
+                alert('Game over ' + player.Name + ' has won!')
+                this.playerList = []
+                this.newPlayerInput = ""
+            }
         }
     }
 })
